0c362eba3c90802c535367728ce0e4f41dfe7289
leminhtri <lmtri1908@gmail.com>
2022-08-01 22:14:11 +0700

    render complete

--- js/mario.js
+++ js/mario.js
@@ -3,6 +3,8 @@ const canvas = document.querySelector('canvas');
 // Tạo môi trường 2d
 const c = canvas.getContext('2d');
 
+// Trọng lực
+const gravity = 1.5
 
 
 // Set Canvas full màn hình  
@@ -22,7 +24,7 @@ class Player {
         // Vận tốc
         this.velocity = {
             x: 0,
-            y: 1
+            y: 0
         }
         // Chiều Cao
         this.width = 30
@@ -38,26 +40,27 @@ class Player {
     // cập nhật lại vị trí
     update() {
         // Trọng lực
-        const gravity = 0.5
+        // const gravity = 0.5
+        this.draw()
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity;
         else
             this.velocity.y = 0
 
-        this.draw()
     }
 }
 
 // Nền Tàng
 class Platform {
-    constructor() {
+    constructor(xVal, yVal) {
         // Vị Trí
         this.position = {
-            x: 300,
-            y: 700
+            x: xVal,
+            y: yVal
         }
 
         this.width = 200
@@ -70,12 +73,17 @@ class Platform {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
+
 //#region
 
 
 
 const player = new Player()
 const platform = new Platform()
+const platforms = [
+    new Platform(200, 100),
+    new Platform(300, 150)
+]
 
 const keys = {
     right: {
@@ -84,35 +92,69 @@ const keys = {
     left: {
         pressed: false,
     },
-    right: {
-        pressed: false,
-    },
-    right: {
-        pressed: false,
-    }
+
 }
 
 //#region Methods
+let scrollOffSet = 0;
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
-    platform.draw();
+
+    // Render platform
+    platforms.forEach(platform => {
+        platform.draw();
+    })
+
 
     // Nếu như bấm nút phải, thì di chuyển sang phải +5, không thì dừng lại
-    if (keys.right.pressed)
+    if (keys.right.pressed && player.position.x < 400)
         player.velocity.x = 5
-    else if (keys.left.pressed)
+    else if (keys.left.pressed && player.position.x > 100)
         player.velocity.x = -5
-    else
+    else {
         player.velocity.x = 0;
+        //  Khi bấm nút chạy sang phải
+        if (keys.right.pressed) {
+            // Tăng Scroll lên 5
+            scrollOffSet += 5;
 
-    //  nếu nhân vật chạm vào platform
-    if (player.position.x + player.height <= platform.position.y &&
-        player.position.y + player.height + player.velocity.y >= platform.position.y &&
-        player.position.x + player.width >= platform.position.x &&
-        player.position.x <= platform.position.x + platform.width)
-        player.velocity.y = 0;
+            // Render platform
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+
+        }
+        //  Khi bấm nút chạy sang trái
+        else if (keys.left.pressed) {
+            // Tăng Scroll lên 5
+            scrollOffSet -= 5;
+
+
+            // Render platform
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+
+        }
+    }
+
+    platforms.forEach(platform => {
+        //  nếu nhân vật chạm vào platform
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width)
+            player.velocity.y = 0;
+    })
+
+
+    // Điều Kiện Để Chiến Thằng Game
+    if(scrollOffSet > 2000){
+        console.log('you win')
+    }
 }
 //#region
 
@@ -138,9 +180,10 @@ window.addEventListener('keydown', ({
             break;
         case 38:
             console.log('up');
+            player.velocity.y -= 10;
             break;
         case 40:
-            player.velocity.y += 10;
+            // player.velocity.y += 20;
             console.log('down');
 
             break;
@@ -162,8 +205,9 @@ window.addEventListener('keyup', ({
             break;
         case 39:
             console.log('right');
+            // player.velocity.x = 0;
             keys.right.pressed = false;
-            // player.velocity.x += 2;
+
             break;
         case 38:
             console.log('up');
